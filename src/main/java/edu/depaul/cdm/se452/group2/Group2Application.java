ce7d38c2791c0cdbaa52e7ac5888e7de1a003150
Louis Rivera <lriver23@depaul.edu>
2022-09-11 12:12:04 -0500

    Add initial spring files

---
+++ src/main/java/edu/depaul/cdm/se452/group2/Group2Application.java
@@ -0,0 +1,13 @@
+package edu.depaul.cdm.se452.group2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Group2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Group2Application.class, args);
+	}
+
+}
