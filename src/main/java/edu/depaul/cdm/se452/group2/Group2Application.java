46bdc223a7519382391f8190d5d330f79fd571a1
Louis Rivera <lriver23@depaul.edu>
2022-09-18 20:48:15 -0500

    Create application configuration files

--- src/main/java/edu/depaul/cdm/se452/group2/Group2Application.java
+++ src/main/java/edu/depaul/cdm/se452/group2/Group2Application.java
@@ -1,13 +1,28 @@
 package edu.depaul.cdm.se452.group2;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
 @SpringBootApplication
 public class Group2Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Group2Application.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner showLogLevel() {
+		return (args) -> {
+			log.debug("Logging Debug");
+			log.info("Logging Info");
+			log.warn("Logging Warning");
+			log.error("Logging Error");
+		};
 	}
 
 }
--- src/main/java/edu/depaul/cdm/se452/group2/inventory/package-info.java
+++ src/main/java/edu/depaul/cdm/se452/group2/inventory/package-info.java
@@ -6,6 +6,10 @@ package edu.depaul.cdm.se452.group2.inventory;
  * 9/11/2022
  * Package info and readme have been created.
  * 
+ * 9/18/2022
+ * I updated the project dependencies so that the project is using the logging, database, 
+ * and webserver talked about in the lecture.
+ * 
  * 
  * 
  */
