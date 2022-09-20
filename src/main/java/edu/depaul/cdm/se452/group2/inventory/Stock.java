d3946a22ff9878b9a957e773734c65c6f517dcbc
Louis Rivera <lriver23@depaul.edu>
2022-09-20 07:00:23 -0500

    Create classes for generating stock tables

---
+++ src/main/java/edu/depaul/cdm/se452/group2/inventory/Stock.java
@@ -0,0 +1,28 @@
+package edu.depaul.cdm.se452.group2.inventory;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "Stocks")
+public class Stock {
+    
+    @Id
+    @GeneratedValue
+    @Column(name = "Id")
+    private long id;
+
+    @OneToOne
+    @Column(name = "Product")
+    private Product product;
+
+    @Column(name = "QuantityAvailable")
+    private int quantityAvailable;
+}
