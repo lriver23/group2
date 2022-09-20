d36ebeae79fc0bf1bfb2ab4d62c70ed0e59f485b
Louis Rivera <lriver23@depaul.edu>
2022-09-19 20:47:46 -0500

    Enable an h2 database on application launch

--- src/main/java/edu/depaul/cdm/se452/group2/inventory/package-info.java
+++ src/main/java/edu/depaul/cdm/se452/group2/inventory/package-info.java
@@ -3,13 +3,28 @@ package edu.depaul.cdm.se452.group2.inventory;
 /*
  * @author Louis Rivera
  * 
+ * ------- Milestone 1 -------
+ * 
  * 9/11/2022
  * Package info and readme have been created.
+ * 
+ * 
+ * ------- Milestone 2 -------
+ * 
+ * Extra Features: 
  * 
  * 9/18/2022
  * I updated the project dependencies so that the project is using the logging, database, 
  * and webserver talked about in the lecture.
  * 
- * I created and populated the resouce files, application-prod, application-dev. I checked that they were working by adding in the test logging statments when the app is launched
+ * I created and populated the resouce files, application-prod, application-dev. 
+ * Ended up checking that they were working by adding in the test logging statments when the app is launched
+ * 
+ * 
+ * 9/19/2022
+ * I created the java classes that will generate the inventory SQL tables: Product classes.
+ * 
+ * I setup the database configuration so that the database is launched when the spring application is started.
+ * 
  * 
  */
