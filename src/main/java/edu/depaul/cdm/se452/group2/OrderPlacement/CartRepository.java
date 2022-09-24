32d75755268e634b3ab6f4e039b42a7b9730ee29
MajidAbdul1 <mmajid3209@gmail.com>
2022-09-24 00:59:03 -0500

    deleted Updated Changes

--- src/main/java/edu/depaul/cdm/se452/group2/OrderPlacement/CartRepository.java
+++
@@ -1,7 +0,0 @@
-package edu.depaul.cdm.se452.group2.OrderPlacement;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CartRepository extends JpaRepository<Cart, Long> {
-
-}
