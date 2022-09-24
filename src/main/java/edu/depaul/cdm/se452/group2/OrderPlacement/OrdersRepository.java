674e645b0d7a49252913126a9504431785c71d4e
MajidAbdul1 <mmajid3209@gmail.com>
2022-09-24 01:20:05 -0500

    Updated

---
+++ src/main/java/edu/depaul/cdm/se452/group2/OrderPlacement/OrdersRepository.java
@@ -0,0 +1,7 @@
+package edu.depaul.cdm.se452.group2.OrderPlacement;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrdersRepository extends JpaRepository<Orders, Long> {
+
+}
