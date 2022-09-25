89b3752940a1d1a474492cb993ace702c77e5363
Louis Rivera <lriver23@depaul.edu>
2022-09-25 12:41:53 -0500

    Update package-info for inventory package

--- src/main/java/edu/depaul/cdm/se452/group2/inventory/package-info.java
+++ src/main/java/edu/depaul/cdm/se452/group2/inventory/package-info.java
@@ -11,21 +11,18 @@ package edu.depaul.cdm.se452.group2.inventory;
  * 
  * ------- Milestone 2 -------
  * 
- * Extra Features:
- * Foreign Keys - Added a foreign key to Product.java that relies on Seller.java.
  * 
  * 9/18/2022
- * I updated the project dependencies so that the project is using the logging, database, 
- * and webserver talked about in the lecture.
+ * Updated the project dependencies to include logging, database, and the webserver.
  * 
- * I created and populated the resouce files, application-prod, application-dev. 
- * Ended up checking that they were working by adding in the test logging statments when the app is launched
+ * Created and populated the resouce files, application-prod, application-dev. 
+ * Added in the test logging statments when the app is launched to test configuration files
  * 
  * 
  * 9/19/2022
- * I created the java classes that will generate the inventory SQL tables: Product, Seller.
+ * Created the java classes that will generate the inventory SQL tables: Product, Seller.
  * 
- * I setup the database configuration so that the database is launched when the spring application is started.
+ * Setup the database configuration so that the database is launched when the spring application is started.
  * 
  * 
  */
