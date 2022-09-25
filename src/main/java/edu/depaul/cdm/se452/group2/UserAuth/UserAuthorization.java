d7bd24d3a85ef0bb34a630b74adde5f6f0d80f02
shoebsmk <your_email_address@example.com>
2022-09-25 18:23:51 -0500

    Added Sql file and fixed errors with tables

--- src/main/java/edu/depaul/cdm/se452/group2/UserAuth/UserAuthorization.java
+++ src/main/java/edu/depaul/cdm/se452/group2/UserAuth/UserAuthorization.java
@@ -8,10 +8,13 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 @Data
 @Entity
+@AllArgsConstructor
+@NoArgsConstructor
 @Table(name = "UserAuthorization")
 public class UserAuthorization {
     //link using primary and secondry keys
