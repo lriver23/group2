49c0d259ed4718c1f4bf8933ea5553ac97b0f7e0
Louis Rivera <lriver23@depaul.edu>
2022-09-25 15:35:56 -0500

    Add test data insert on app launch

---
+++ src/main/resources/data.sql
@@ -0,0 +1,11 @@
+-- Inventory Package
+
+INSERT INTO Products (Name, Description, Price) VALUES ('Deck of Cards', 'A deck of 52 playing cards.', 5.15)
+INSERT INTO Products (Name, Description, Price) VALUES ('A Set of Dice', '15 ordinary dice for all occasions.', 9.90)
+INSERT INTO Products (Name, Description, Price) VALUES ('Dos', 'The sequel to everyones favorite card game.', 5.15)
+INSERT INTO Products (Name, Description, Price) VALUES ('80 screws', 'A box of standardized screws.', 2.80)
+INSERT INTO Products (Name, Description, Price) VALUES ('Deluxe Hammer', 'A hammer with all the bells and wistles.', 85.00)
+INSERT INTO Products (Name, Description, Price) VALUES ('Carpenters Bench', 'A bench that no carpenter can live without.', 299.99)
+
+INSERT INTO Sellers (Name, Income) VALUES ('Dice and Games Industires', 6000000)
+INSERT INTO Sellers (Name, Income) VALUES ('World Wide Tools', 85)
