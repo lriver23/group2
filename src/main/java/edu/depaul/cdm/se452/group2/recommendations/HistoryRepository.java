7d234e1099421d3c824aa55e7f2f0d6cb5599325
Mally <mtzuoo@gmail.com>
2022-09-27 14:51:10 -0500

    update for milestone 2

--- src/main/java/edu/depaul/cdm/se452/group2/recommendations/HistoryRepository.java
+++
@@ -1,7 +0,0 @@
-package edu.depaul.cdm.se452.group2.recommendations;
- 
-import org.springframework.data.jpa.repository.JpaRepository;
- 
-public interface HistoryRepository extends JpaRepository<History, Long> {
-  
-}
--- src/main/java/edu/depaul/cdm/se452/group2/recommendations/package-info.java
+++ src/main/java/edu/depaul/cdm/se452/group2/recommendations/package-info.java
@@ -6,4 +6,14 @@ package edu.depaul.cdm.se452.group2.recommendations;
  * 9/12/2022
  * Package info and readme have been created.
  * 
+ * ------- Milestone 2 -------
+ * 
+ * Extra Features:
+ * 
+ * 9/26/2022
+ * Made java class history service 
+ * 
+ * 9/27/2022
+ * Added Tests for history tables along with sql scripts
+ * 
  */
