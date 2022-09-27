492c16f1f9503b2daa048792f976ba417c89db02
shoebsmk <your_email_address@example.com>
2022-09-27 02:53:49 -0500

    Fixed issues with sql file

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,12 @@
+INSERT INTO Products (Name, Description, Price) VALUES ('Deck of Cards', 'A deck of 52 playing cards.', 5.15);
+INSERT INTO Products (Name, Description, Price) VALUES ('A Set of Dice', '15 ordinary dice for all occasions.', 9.90);
+INSERT INTO Products (Name, Description, Price) VALUES ('Dos', 'The sequel to everyones favorite card game.', 5.15);
+INSERT INTO Products (Name, Description, Price) VALUES ('80 screws', 'A box of standardized screws.', 2.80);
+INSERT INTO Products (Name, Description, Price) VALUES ('Deluxe Hammer', 'A hammer with all the bells and wistles.', 85.00);
+INSERT INTO Products (Name, Description, Price) VALUES ('Carpenters Bench', 'A bench that no carpenter can live without.', 299.99);
+
+INSERT INTO Sellers (Name, Income) VALUES ('Dice and Games Industires', 6000000);
+INSERT INTO Sellers (Name, Income) VALUES ('World Wide Tools', 85);
 
 insert into Cart(Cart_ID , Order_ID, Product_Quantity ) values (111 , 555, 2 );
 insert into Cart(Cart_ID , Order_ID, Product_Quantity ) values (222  , 666, 4);
