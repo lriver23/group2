4d42a9f302eac49b0516c4d00d83217af4efe5e6
shoebsmk <your_email_address@example.com>
2022-09-27 02:29:37 -0500

    Hello

--- src/main/java/edu/depaul/cdm/se452/group2/UserAuth/Authentication.java
+++ src/main/java/edu/depaul/cdm/se452/group2/UserAuth/Authentication.java
@@ -1,4 +1,4 @@
-package edu.depaul.cdm.se452.group2.userAuth;
+package edu.depaul.cdm.se452.group2.UserAuth;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
