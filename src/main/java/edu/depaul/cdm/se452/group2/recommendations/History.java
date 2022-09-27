2de32a16bc3e5f3701f4d740a35c03e350a2d7a4
Mally <mtzuoo@gmail.com>
2022-09-27 11:06:50 -0500

    Changed history to what it is in main

--- src/main/java/edu/depaul/cdm/se452/group2/recommendations/History.java
+++ src/main/java/edu/depaul/cdm/se452/group2/recommendations/History.java
@@ -6,17 +6,22 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
 import lombok.Data;
- 
+import lombok.NoArgsConstructor;
+
 @Data
 @Entity
+@AllArgsConstructor
+@NoArgsConstructor
 @Table(name = "History")
 public class History {
   
    @Id
    @GeneratedValue(strategy = GenerationType.IDENTITY)
    @Column(name = "Search")
-   private long Search;
+   private String Search;
  
    @Column(name = "ItemList")
    private String ItemList;
