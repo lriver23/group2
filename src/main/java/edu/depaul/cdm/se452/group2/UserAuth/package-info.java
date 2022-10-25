fbd5bde33bba267b693258fecf38f4edf5343df0
Shoeb Mohammed Khan <42929978+shoebsmk@users.noreply.github.com>
2022-10-25 04:00:06 -0500

    Added package info

--- src/main/java/edu/depaul/cdm/se452/group2/UserAuth/package-info.java
+++ src/main/java/edu/depaul/cdm/se452/group2/UserAuth/package-info.java
@@ -23,4 +23,23 @@ package edu.depaul.cdm.se452.group2.UserAuth;
  * I added Tests for both the Authentication and Authoriztion tables
  * For extra features I added NoArgConstructors and AllArgConstructors as extra features.
  * 
+ * 
+ * 
+ * ------- Milestone 2 -------
+ * Oct 16, 2022
+ * Added services for Authenticatoin and Authorization tables
+ * 
+ * Oct 20, 2022
+ * Added one to one mapping for Authenticatoin and Authorization tables
+ * Restructured files in sub folders and added validation services
+ * 
+ * Oct 21, 2022
+ * Added Address and Login entities, repos and thier services and set-up relationships among all entities.
+ *  
+ * Oct 21, 2022 (Extra Feature)
+ * Added extra logic to add rows in other tables with null values for unassigned data when main(Authenticatoin) table is created
+ * Added login logic which can verify credentials and set login status in boolean value (Initially false)
+ * Added logs
+ * 
+ * 
  */
