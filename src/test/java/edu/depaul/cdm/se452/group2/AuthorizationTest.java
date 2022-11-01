12c986f758ce8e6167e7e094880d1fc7f78de3c8
Shoeb Mohammed Khan <42929978+shoebsmk@users.noreply.github.com>
2022-11-01 18:41:58 -0500

    Fixed Authorization test.

--- src/test/java/edu/depaul/cdm/se452/group2/AuthorizationTest.java
+++ src/test/java/edu/depaul/cdm/se452/group2/AuthorizationTest.java
@@ -1,26 +1,37 @@
-// package edu.depaul.cdm.se452.group2;
-// import org.junit.jupiter.api.Test;
-// import lombok.val;
-// import static org.junit.jupiter.api.Assertions.assertEquals;
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.boot.test.context.SpringBootTest;
+package edu.depaul.cdm.se452.group2;
+import org.junit.jupiter.api.Test;
+//import lombok.var;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-// import edu.depaul.cdm.se452.group2.UserAuth.AuthorizationRepo;
-// import edu.depaul.cdm.se452.group2.UserAuth.AuthorizationU;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
-// @SpringBootTest
-// public class AuthorizationTest {
-//     @Autowired
-//     private AuthorizationRepo authorizationRepo;
+import edu.depaul.cdm.se452.group2.UserAuth.repos.AuthenticationRepo;
+import edu.depaul.cdm.se452.group2.UserAuth.repos.AuthorizationRepo;
+import edu.depaul.cdm.se452.group2.UserAuth.entities.Authentication;
+import edu.depaul.cdm.se452.group2.UserAuth.entities.AuthorizationU;
 
-//     @Test
-//     public void testAuthorization() {
-//         val row1 = new AuthorizationU("Shoeb",true, true );
-//         val row2 = new AuthorizationU("Majid",false, false );
-//         val before = authorizationRepo.count();
-//         authorizationRepo.save(row1);
-//         authorizationRepo.save(row2);
-//         val after = authorizationRepo.count();
-//         assertEquals(before + 2, after);
-//     }
-// }
+@SpringBootTest
+public class AuthorizationTest {
+
+    @Autowired
+    private AuthorizationRepo authorizationRepo;
+
+    @Autowired
+    AuthenticationRepo authenticationRepo;
+
+    @Test
+    public void testAuthorization() {
+        var row1 = new AuthorizationU();
+        var auth1 = new Authentication();
+        auth1.setU_name("John");
+        authenticationRepo.save(auth1);
+        row1.setU_name(auth1);
+
+        var before = authorizationRepo.count();
+        authorizationRepo.save(row1);
+        
+        var after = authorizationRepo.count();
+        assertEquals(before + 1, after);
+    }
+}
