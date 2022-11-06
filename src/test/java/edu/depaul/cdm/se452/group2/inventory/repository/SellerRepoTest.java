421e3d5dc117e7b64e817a54f8cddf585d9401db
Louis Rivera <lriver23@depaul.edu>
2022-11-06 17:56:21 -0600

    Fix broken inventory tests

--- src/test/java/edu/depaul/cdm/se452/group2/inventory/repository/SellerRepoTest.java
+++ src/test/java/edu/depaul/cdm/se452/group2/inventory/repository/SellerRepoTest.java
@@ -1,49 +1,60 @@
-// package edu.depaul.cdm.se452.group2.inventory.repository;
+package edu.depaul.cdm.se452.group2.inventory.repository;
 
-// import static org.junit.jupiter.api.Assertions.assertEquals;
-// import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
-// import org.junit.jupiter.api.Test;
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
-// import edu.depaul.cdm.se452.group2.inventory.Seller;
-// import edu.depaul.cdm.se452.group2.inventory.SellerRepository;
+import edu.depaul.cdm.se452.group2.inventory.ProductRepository;
+import edu.depaul.cdm.se452.group2.inventory.Seller;
+import edu.depaul.cdm.se452.group2.inventory.SellerRepository;
 
-// @SpringBootTest
-// public class SellerRepoTest {
-//     @Autowired
-//     private SellerRepository repo;
+@SpringBootTest
+public class SellerRepoTest {
+    @Autowired
+    private SellerRepository sellerRepo;
 
-//     @Test
-//     public void testCrud() {
-//         var seller = new Seller();
-//         seller.setIncome(4003.12);
-//         seller.setName("Sales Inc.");
+    @Autowired
+    private ProductRepository productRepo;
 
-//         var initialSellerId = seller.getId();  
-//         var initialRepoCount = repo.count();
+    @BeforeEach
+    public void setup() {
+        productRepo.deleteAll();
+        sellerRepo.deleteAll();
+    }
+    
+    @Test
+    public void testCrud() {
+        var seller = new Seller();
+        seller.setIncome(4003.12);
+        seller.setName("Sales Inc.");
 
-//         repo.save(seller);
-//         var endSellerId = seller.getId();
-//         var endRepoCount = repo.count();
+        var initialSellerId = seller.getId();  
+        var initialRepoCount = sellerRepo.count();
 
-//         assertNotEquals(initialSellerId, endSellerId);
-//         assertEquals(initialRepoCount + 1, endRepoCount);
+        sellerRepo.save(seller);
+        var endSellerId = seller.getId();
+        var endRepoCount = sellerRepo.count();
 
-//         var repoSeller = repo.findById(endSellerId).orElse(new Seller());
-//         var updatedIncome = 220.45;
-//         repoSeller.setIncome(updatedIncome);
+        assertNotEquals(initialSellerId, endSellerId);
+        assertEquals(initialRepoCount + 1, endRepoCount);
 
-//         repo.save(repoSeller);
-//         var updatedSeller = repo.findById(endSellerId).orElse(new Seller());
+        var repoSeller = sellerRepo.findById(endSellerId).orElse(new Seller());
+        var updatedIncome = 220.45;
+        repoSeller.setIncome(updatedIncome);
+
+        sellerRepo.save(repoSeller);
+        var updatedSeller = sellerRepo.findById(endSellerId).orElse(new Seller());
         
-//         assertNotEquals(seller, updatedSeller);
-//         assertEquals(updatedSeller.getIncome(), updatedIncome);
+        assertNotEquals(seller, updatedSeller);
+        assertEquals(updatedSeller.getIncome(), updatedIncome);
 
-//         initialRepoCount = repo.count();
-//         repo.delete(updatedSeller);
-//         endRepoCount = repo.count();
-//         assertEquals(initialRepoCount - 1, endRepoCount);
-//     }   
-// }
+        initialRepoCount = sellerRepo.count();
+        sellerRepo.delete(updatedSeller);
+        endRepoCount = sellerRepo.count();
+        assertEquals(initialRepoCount - 1, endRepoCount);
+    }   
+}
