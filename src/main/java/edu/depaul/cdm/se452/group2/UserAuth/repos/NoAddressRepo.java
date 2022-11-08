9861a6cb63cfc2cf5688860f8b808cba91480371
Shoeb Mohammed Khan <42929978+shoebsmk@users.noreply.github.com>
2022-11-08 05:19:34 -0600

    Rename did not sync with github so, deleted folder

--- src/main/java/edu/depaul/cdm/se452/group2/UserAuth/repos/NoAddressRepo.java
+++
@@ -1,9 +0,0 @@
-package edu.depaul.cdm.se452.group2.userAuth.repos;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import edu.depaul.cdm.se452.group2.userAuth.entities.NoAddress;
-
-public interface NoAddressRepo extends MongoRepository<NoAddress, Long> {
-
-    
-}
